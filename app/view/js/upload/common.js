91e4f435fd504a4b0933d65c74fb2e253c4c45ba
Andrew Radev <andrey.radev@gmail.com>
2026-03-12 11:58:03 +0100

    Better handling of scrolling and tooltips, remove form icons

--- app/view/js/upload/common.js
+++ app/view/js/upload/common.js
@@ -1,12 +1,17 @@
 Page('.upload-page .step-content.active', function($page) {
+  // Don't do any fancy scrolling on page reload, if possible:
+  let isReload =
+    window.performance.getEntriesByType('navigation')[0]?.type === 'reload' ||
+    window.performance.navigation?.type === 1; // fallback for old browsers
+
   // If there is an error, scroll to it:
   let $errorMessageList = $page.find('.error-message-list');
   if ($errorMessageList.length > 0) {
     setTimeout(function() {
       $(document).scrollTo($errorMessageList, 500, {offset: -20});
     }, 1);
-  } else {
-    // If there is a previous section status, scroll to it:
+  } else if (!isReload) {
+    // If there is a previous section status, scroll to it, unless the user refreshed the page:
     let $previousSection = $page.prev('.step-content');
     if ($previousSection.length > 0) {
       setTimeout(function() {
