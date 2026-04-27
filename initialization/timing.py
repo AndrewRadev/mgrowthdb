cde1d768eef82402d5c26b0254c2092587142db5
Andrew Radev <andrey.radev@gmail.com>
2026-04-27 12:03:50 +0200

    Avoid potential problem with f-string

--- initialization/timing.py
+++ initialization/timing.py
@@ -68,7 +68,8 @@ def _log_db_timing(conn, cursor, statement, parameters, context, executemany):
 
     if os.getenv('TIME'):
         logger = current_app.logger.getChild('timing')
-        logger.info(f"[{duration_ms}ms] Query: {' '.join(statement.split('\n'))}")
+        query = ' '.join(statement.split('\n'))
+        logger.info(f"[{duration_ms}ms] Query: {query}")
 
 
 def _is_static(request):
