d5f036f9448c6940960b88c2dbef84a53ade47e4
Andrew Radev <andrey.radev@gmail.com>
2026-05-15 12:18:41 +0200

    Construct workspace entry records

--- app/model/orm/workspace_entry.py
+++ app/model/orm/workspace_entry.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import itertools
 
 import pandas as pd
 import sqlalchemy as sql
@@ -33,6 +34,46 @@ class WorkspaceEntry(OrmBase):
 
     userId: Mapped[int] = mapped_column(sql.ForeignKey('Users.id'), nullable=False)
     user: Mapped['User'] = relationship(back_populates="workspaceEntries")
+
+    @classmethod
+    def from_csv(Self, file, user, metadata={}, include_error=False):
+        """
+        Construct workspace entry records from the data in a CSV file.
+
+        The first column will be parsed as time values, every other column will
+        be considered to represent measurements. If `include_error` is truthy,
+        columns will be parsed as pairs of value and error measurements.
+        """
+        df = pd.read_csv(file)
+        time_col = df.columns[0]
+
+        if include_error:
+            value_and_error_columns = list(itertools.batched(df.columns[1:], 2))
+        else:
+            value_and_error_columns = [(c, None) for c in df.columns[1:]]
+
+        entries = []
+
+        for (value_column, error_column) in value_and_error_columns:
+            if error_column is None:
+                subset = [time_col, value_column]
+            else:
+                subset = [time_col, value_column, error_column]
+
+            csv_data = df[subset].rename(columns={
+                time_col: 'time',
+                value_column: 'value',
+                error_column: 'error',
+            }).to_csv(index=False)
+
+            entries.append(Self(
+                label=value_column,
+                user=user,
+                data=csv_data,
+                **metadata,
+            ))
+
+        return entries
 
     def get_df(self):
         return pd.read_csv(BytesIO(self.data.encode('utf-8')))
---
+++ tests/model/orm/test_workspace_entry.py
@@ -0,0 +1,67 @@
+import tests.init  # noqa: F401
+
+import unittest
+from io import StringIO
+
+import pandas as pd
+
+from app.model.orm import WorkspaceEntry
+from tests.database_test import DatabaseTest
+
+
+class TestWorkspaceEntry(DatabaseTest):
+    def test_create_from_csv(self):
+        user = self.create_user()
+
+        # Without error columns:
+        df = pd.DataFrame.from_dict({
+            'Time': [1, 2, 3],
+            'Roseburia': [1e6, 1e7, 1e8],
+            'Blautia': [1e6, 1e7, 1e8],
+        })
+        csv_file = StringIO(df.to_csv(index=False))
+        entries = WorkspaceEntry.from_csv(csv_file, user)
+
+        self.assertEqual([e.label for e in entries], ['Roseburia', 'Blautia'])
+        self.assertEqual(entries[0].user, user)
+        self.assertEqual(entries[0].get_df().columns.tolist(), ['time', 'value'])
+
+        # With error columns:
+        df = pd.DataFrame.from_dict({
+            'time': [1, 2, 3],
+            'glucose': [10, 9, 8],
+            'glucose STD': [0.1, 0.1, 0.1],
+            'trehalose': [10, 9, 8],
+            'trehalose STD': [0.1, 0.1, 0.1],
+        })
+        csv_file = StringIO(df.to_csv(index=False))
+        entries = WorkspaceEntry.from_csv(csv_file, user, include_error=True)
+
+        self.assertEqual([e.label for e in entries], ['glucose', 'trehalose'])
+        self.assertEqual(entries[0].user, user)
+        self.assertEqual(entries[0].get_df().columns.tolist(), ['time', 'value', 'error'])
+
+        # With additional metadata
+        df = pd.DataFrame.from_dict({
+            'time': [1, 2, 3],
+            'glucose': [10, 9, 8],
+            'trehalose': [10, 9, 8],
+        })
+        csv_file = StringIO(df.to_csv(index=False))
+        entries = WorkspaceEntry.from_csv(
+            csv_file,
+            user,
+            metadata={
+                'dataType': 'measurement',
+                'subjectType': 'metabolite',
+                'units': 'mM',
+            }
+        )
+
+        self.assertEqual([e.dataType for e in entries], ['measurement', 'measurement'])
+        self.assertEqual([e.subjectType for e in entries], ['metabolite', 'metabolite'])
+        self.assertEqual([e.units for e in entries], ['mM', 'mM'])
+
+
+if __name__ == '__main__':
+    unittest.main()
