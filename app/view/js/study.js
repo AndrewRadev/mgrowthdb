cb66e9cb8e841f8ad8cfbb02a754671dd043902d
Andrew Radev <andrey.radev@gmail.com>
2026-07-24 11:51:21 +0200

    Render fieldset around experiment filter

--- app/view/js/study.js
+++ app/view/js/study.js
@@ -1,14 +1,19 @@
 Page('.study-page', function($page) {
   initCompareButtons($page);
 
-  $experiments = $('.js-experiments');
+  let $experiments = $('.js-experiments');
+  let select2_selectors = '.js-strain-select,.js-metabolite-select,.js-modeling-type-select'
 
   if ($experiments.length > 0) {
     let $searchForm  = $page.find('.js-search-form');
 
     $page.on('keyup', 'input[name=q]', _.debounce(updateSearch, 200));
-    $page.on('change', '.js-strain-select,.js-metabolite-select,.js-modeling-type-select', updateSearch)
+    $page.on('change', select2_selectors, updateSearch)
     $searchForm.on('reset', function() {
+      $page.find(select2_selectors).each(function() {
+        $(this).val(null).trigger('change');
+      });
+
       setTimeout(updateSearch, 1);
     });
 
@@ -16,7 +21,7 @@ Page('.study-page', function($page) {
     setTimeout(updateSearch, 1);
 
     // Initialize select filters:
-    $page.find('.js-strain-select,.js-metabolite-select,.js-modeling-type-select').each(function() {
+    $page.find(select2_selectors).each(function() {
       let $select = $(this);
 
       $select.select2({
