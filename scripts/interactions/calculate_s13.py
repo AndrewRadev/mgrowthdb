7c8d35043774a943043e627603c5e227ca2a3fee
Andrew Radev <andrey.radev@gmail.com>
2026-08-13 17:07:24 +0200

    Work-in-progress scripts to generate interaction charts

---
+++ scripts/interactions/calculate_s13.py
@@ -0,0 +1,125 @@
+import json
+import requests
+from io import BytesIO
+
+import numpy as np
+import pandas as pd
+from scipy.stats import ttest_ind
+
+from scripts.interactions.functions import calculate_api_interactions, pp
+
+root_url = 'http://localhost:8081/api/v1'
+# root_url = 'https://mgrowthdb.gbiomed.kuleuven.be/api/v1'
+
+def get_json(endpoint):
+    response = requests.get(f"{root_url}/{endpoint}.json")
+    response.raise_for_status()
+    return response.json()
+
+
+def get_df(endpoint):
+    response = requests.get(f"{root_url}/{endpoint}.csv")
+    response.raise_for_status()
+    return pd.read_csv(BytesIO(response.content))
+
+
+experiment_id_data = get_json("study/SMGDB00000013")["experiments"]
+experiment_data = [get_json(f"experiment/{e["id"]}") for e in experiment_id_data]
+
+strain_associations = [
+    (
+        "Agrobacterium tumefaciens str. MWF001",
+        "Agrobacterium tumefaciens str. MWF001 plate counts",
+        ["At", "At+Ct", "At+Oa", "At+Ms"],
+    ),
+    (
+        "Comamonas testosteroni str. MWF001",
+        "Comamonas testosteroni str. MWF001 plate counts",
+        ["Ct", "At+Ct", "CtOa", "MsOa"],
+    ),
+    (
+        "Microbacterium saperdae str. MWF001",
+        "Microbacterium saperdae str. MWF001 plate counts",
+        ["Ms", "At+Ms", "Ct+Ms", "MsOa"],
+    ),
+    (
+        "Ochrobactrum anthropi str. MWF001",
+        "Ochrobactrum anthropi str. MWF001 plate counts",
+        ["Oa", "At+Oa", "CtOa", "MsOa"],
+    ),
+]
+
+interactions = calculate_api_interactions(
+    metric='auc',
+    experiment_data=experiment_data,
+    strain_associations=strain_associations,
+)
+
+pp(interactions)
+exit()
+
+mono_bt_id = next(e["id"] for e in experiments if e['name'] == 'BT_WC')
+mono_ri_id = next(e["id"] for e in experiments if e['name'] == 'RI_WC')
+co_btri_id = next(e["id"] for e in experiments if e['name'] == 'BTRI_WC')
+
+bt_mono = []
+for bioreplicate in get_json(f"experiment/{mono_bt_id}")["bioreplicates"]:
+    # Skip average bioreplicates, we want to only work with observational data:
+    if bioreplicate['isAverage']:
+        continue
+
+    for mc in bioreplicate['measurementContexts']:
+        if mc['subject']['name'].startswith('Bacteroides'):
+            assert(mc['growthRate'] is not None)
+            bt_mono.append(float(mc['growthRate']))
+
+ri_mono = []
+for bioreplicate in get_json(f"experiment/{mono_ri_id}")["bioreplicates"]:
+    # Skip average bioreplicates, we want to only work with observational data:
+    if bioreplicate['isAverage']:
+        continue
+
+    for mc in bioreplicate['measurementContexts']:
+        if mc['subject']['name'].startswith('Roseburia'):
+            assert(mc['growthRate'] is not None)
+            ri_mono.append(float(mc['growthRate']))
+
+bt_co = []
+ri_co = []
+for bioreplicate in get_json(f"experiment/{co_btri_id}")["bioreplicates"]:
+    # Skip average bioreplicates, we want to only work with observational data:
+    if bioreplicate['isAverage']:
+        continue
+
+    for mc in bioreplicate['measurementContexts']:
+        if mc['subject']['name'].startswith('Bacteroides'):
+            assert(mc['growthRate'] is not None)
+            bt_co.append(float(mc['growthRate']))
+
+        elif mc['subject']['name'].startswith('Roseburia'):
+            assert(mc['growthRate'] is not None)
+            ri_co.append(float(mc['growthRate']))
+
+print(bt_mono)
+print(bt_co)
+print(ri_mono)
+print(ri_co)
+
+bt_log = np.log(np.mean(bt_co) / np.mean(bt_mono))
+ri_log = np.log(np.mean(ri_co) / np.mean(ri_mono))
+
+print("BT co:")
+print(bt_co)
+print("BT mono:")
+print(bt_mono)
+print(f"BT log-ratio: {bt_log}")
+bt_t_test = ttest_ind(bt_co, bt_mono)
+print(f"BT p-value: {bt_t_test.pvalue}")
+
+print("RI co:")
+print(ri_co)
+print("RI mono:")
+print(ri_mono)
+print(f"RI log-ratio: {ri_log}")
+ri_t_test = ttest_ind(ri_co, ri_mono)
+print(f"RI p-value: {ri_t_test.pvalue}")
---
+++ scripts/interactions/functions.py
@@ -0,0 +1,145 @@
+import json
+
+import numpy as np
+from scipy.stats import ttest_ind, zscore
+
+
+def pp(input):
+    print(json.dumps(input, indent=2))
+
+
+def calculate_api_interactions(metric, experiment_data, strain_associations):
+    """
+    Calculate interactions between the given strains from experiment data
+    retrieved though the API
+
+    :param metric: Either "growthRate" or "auc"
+    :param experiment_data:
+        Experimental metadata retrieved from the API that is to be filtered to
+        find strain interactions. This does not include individual
+        measurements, only metadata and aggregate metrics.
+    :param strain_associations:
+        List of tuples, each of which has the form:
+        (<strain name>, <measurement label>, <list of experiment names>)
+    """
+    assert(metric in ('growthRate', 'auc'))
+
+    # Filter experiment data: for each experiment, pluck target measurements only, group by name
+    # Identify mono and co-culture experiments
+
+    strains, mono, pairs = _collect_cultures(metric, experiment_data, strain_associations)
+    pairs = _calculate_ratios(strains, mono, pairs)
+
+    _save_chart("tmp.dot", pairs)
+
+    return pairs
+
+
+def _collect_cultures(metric, experiment_data, strain_associations):
+    pairs = {}
+    mono = {}
+    all_strains = set()
+
+    for (strain, _, _) in strain_associations:
+        all_strains.add(strain)
+        mono[strain] = []
+
+        for (other_strain, _, _) in strain_associations:
+            if strain == other_strain:
+                continue
+            key = f"{strain} with {other_strain}"
+            pairs[key] = []
+
+    for (strain, measurement_label, experiment_names) in strain_associations:
+        mono_measurements = []
+        co_measurements = []
+
+        for experiment in experiment_data:
+            experiment_strain_names = {s['name'] for s in experiment['communityStrains']}
+            experiment_strain_names = all_strains.intersection(experiment_strain_names)
+
+            if len(experiment_strain_names) == 0:
+                continue
+
+            if len(experiment_strain_names) == 1:
+                other_strain = None
+            elif len(experiment_strain_names) == 2:
+                other_strain = list(experiment_strain_names - {strain})[0]
+            else:
+                raise ValueError(f"Given experiment includes {len(strain_names)} strains")
+
+            for bioreplicate in experiment['bioreplicates']:
+                # Ignore computed bioreplicates
+                if bioreplicate['isAverage']:
+                    continue
+
+                for measurement_context in bioreplicate['measurementContexts']:
+                    # Only target the requested technique:
+                    if measurement_context['label'] != measurement_label:
+                        continue
+
+                    value = measurement_context[metric]
+
+                    if other_strain:
+                        pairs[f"{strain} with {other_strain}"].append(value)
+                    else:
+                        mono[strain].append(value)
+
+    return all_strains, mono, pairs
+
+
+def _calculate_ratios(strains, mono, pairs):
+    ratios = []
+
+    for strain in strains:
+        for other_strain in strains:
+            if strain == other_strain:
+                continue
+
+            pair_values = pairs[f"{strain} with {other_strain}"]
+            mono_values = mono[strain]
+
+            log_ratio = float(np.log(np.mean(pair_values) / np.mean(mono_values)))
+            p_value = ttest_ind(pair_values, mono_values).pvalue
+
+            if p_value < 0.001:
+                p_symbol = '***'
+            elif p_value < 0.01:
+                p_symbol = '**'
+            elif p_value < 0.05:
+                p_symbol = '*'
+            else:
+                p_symbol = ''
+
+            ratios.append((strain, other_strain, log_ratio, p_value, p_symbol))
+
+    return ratios
+
+
+def _save_chart(filename, pairs):
+    with open(filename, 'w') as f:
+        print("""
+            digraph G {
+            graph [layout=dot rankdir=TD]
+            node  [shape=box style=rounded]
+        """, file=f)
+
+        abs_log_ratios = [np.abs(log_ratio) for (_, _, log_ratio, _, _) in pairs]
+        z_scores = zscore(abs_log_ratios)
+
+        print(z_scores)
+
+        for i, (first, second, log_ratio, p_value, p_symbol) in enumerate(pairs):
+            if p_symbol == "":
+                continue
+
+            size = z_scores[i] + 1
+
+            if log_ratio > 0:
+                color = "darkgreen"
+            elif log_ratio <= 0:
+                color = "brown"
+
+            print(f"\"{first}\" -> \"{second}\" [label=\"{p_symbol}\", penwidth=\"{size}\", color=\"{color}\"]", file=f)
+
+        print("}", file=f)
